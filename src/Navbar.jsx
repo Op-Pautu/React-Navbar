5a5dc3a4a8fc54bc191c3f8a8b5d21feb5948854
Op Pautu <pautuop@gmail.com>
2023-03-04 11:26:03 +0530

    Add navbar and basic style

---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <a href="/" className="site-title">
+        Home
+      </a>
+      <ul>
+        <li>
+          <a href="/pricing">Pricing</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
